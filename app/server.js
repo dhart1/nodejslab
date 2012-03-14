3a35877560eeb3e34b842fcd1383bdc3d79f1722
David Hart <dhart1@unca.edu>
2012-03-14 10:45:05 -0400

    added redis database

--- app/server.js
+++ app/server.js
@@ -1,8 +1,36 @@
 var http = require('http');
+var redis = require('redis');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!\n');
+var client = redis.createClient();
+
+
+	/*client.mget{['awesome','gnarly'], function(error, responses){
+		console.log(responses);
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('awesomeCount: '+responses[0] +'<br /> gnarlyCount: '+responses[1]);
+	});*/
+	
+http.createServer(function (req, res) {	
+	client.get('awesome', function(error, awesomeCount){
+		client.get('cool', function(error, coolCount){
+			client.get('rad', function(error, radCount){
+				client.get('gnarly', function(error, gnarlyCount){
+					client.get('groovy', function(error, groovyCount){
+	    				if(error) {
+							awesomeCount=error;
+	    				}
+						res.writeHead(200, {'Content-Type': 'text/html'});
+	    				res.end('awesomeCount: '+awesomeCount +'<br />'+
+								'coolCount: '+ coolCount +'<br />'+
+								'radCount: '+ radCount +'<br />'+
+								'gnarlyCount: '+ gnarlyCount +'<br />'+
+								'groovyCount: '+ groovyCount
+						);		
+					});
+				});
+			});
+		});
+	});
 }).listen(3000);
 
 console.log('Server running on port 3000');
--- app/twitter.js
+++ app/twitter.js
@@ -1,5 +1,17 @@
 var twitter = require('ntwitter');
+var redis = require('redis');
 var credentials = require('./credentials.js');
+
+var client = redis.createClient();
+
+/*client.exists('awesome', function(error, exists) {
+	if (error) {
+		console.log('ERROR: '+error);
+	} else if(!exists) {
+		client.set('awesome',0);
+	};
+});*/
+
 
 var t = new twitter({
     consumer_key: credentials.consumer_key,
@@ -14,6 +26,22 @@ t.stream(
     function(stream) {
         stream.on('data', function(tweet) {
             console.log(tweet.text);
+
+			if(tweet.text.match(/awesome/)) {
+				client.incr('awesome');
+			};
+			if(tweet.text.match(/cool/)) {
+				client.incr('cool');
+			};
+			if(tweet.text.match(/rad/)) {
+				client.incr('rad');
+			};
+			if(tweet.text.match(/gnarly/)) {
+				client.incr('gnarly');
+			};
+			if(tweet.text.match(/groovy/)) {
+				client.incr('groovy');
+			};
         });
     }
 );
